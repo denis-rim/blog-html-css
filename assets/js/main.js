7a9832774cb682bf447ad6d303175ed44fd0c1fd
Denis Mazurchuk <denis.aurorim@gmail.com>
2022-03-07 21:43:07 +0200

    Change Navbar styles on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
 // Grab elements
+const selectElement = (select) => {
+  const element = document.querySelector(select);
+
+  if (element) {
+    return element;
+  } else {
+    throw new Error(`Element ${select} not found`);
+  }
+};
 
 //Nav styles on scroll
+const scrollHeader = () => {
+  const headerElement = selectElement("#header");
+  if (this.scrollY >= 15) {
+    headerElement.classList.add("activated");
+  } else {
+    headerElement.classList.remove("activated");
+  }
+};
 
+window.addEventListener("scroll", scrollHeader);
 // Open menu & search pop-up
 
 // Open/Close search form popup
