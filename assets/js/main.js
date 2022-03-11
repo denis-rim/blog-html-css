6e3a53b2429f490525c58eacda477cafdf417461
Denis Mazurchuk <denis.aurorim@gmail.com>
2022-03-11 19:18:30 +0200

    Add Swiper carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,3 +71,34 @@ themeToggleBtn.addEventListener("click", () => {
 });
 
 // Swiper
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
